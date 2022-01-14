232d0a6590ffaa27cd385344b748c45ff0a056e1
Oliver Coimbra <olivercoimbra@gmail.com>
2022-01-14 12:14:30 -0400

    Change to use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,175 +1,117 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
 import './calculator.css';
-import calculate from '../logic/calculate';
+import '../logic/operate';
+import PropTypes from 'prop-types';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: 0,
-    };
-    this.handleClick = this.handleClick.bind(this);
+const Calculator = (props) => {
+  const { calcStates, handleClick } = props;
+  const { total, next } = calcStates;
+  let val = 0;
+  if (next) {
+    val = next;
+  } else if (total) {
+    val = total;
   }
 
-  handleClick(e) {
-    this.setState((previousState) => calculate(previousState, e.target.innerText));
-  }
+  return (
+    <div className="container">
+      <input
+        className="grid-item display"
+        placeholder="0"
+        value={val}
+        readOnly
+      />
+      <button type="button" className="grid-item AC" onClick={handleClick}>
+        AC
+      </button>
+      <button
+        type="button"
+        className="grid-item operations"
+        onClick={handleClick}
+      >
+        +/-
+      </button>
+      <button
+        type="button"
+        className="grid-item procentage"
+        onClick={handleClick}
+      >
+        %
+      </button>
+      <button
+        type="button"
+        className="grid-item divide operation"
+        onClick={handleClick}
+      >
+        ÷
+      </button>
+      <button type="button" className="grid-item seven" onClick={handleClick}>
+        7
+      </button>
+      <button type="button" className="grid-item eight" onClick={handleClick}>
+        8
+      </button>
+      <button type="button" className="grid-item nine" onClick={handleClick}>
+        9
+      </button>
+      <button
+        type="button"
+        className="grid-item multiply operation"
+        onClick={handleClick}
+      >
+        ×
+      </button>
+      <button type="button" className="grid-item four" onClick={handleClick}>
+        4
+      </button>
+      <button type="button" className="grid-item five" onClick={handleClick}>
+        5
+      </button>
+      <button type="button" className="grid-item six" onClick={handleClick}>
+        6
+      </button>
+      <button
+        type="button"
+        className="grid-item minus operation"
+        onClick={handleClick}
+      >
+        -
+      </button>
+      <button type="button" className="grid-item one" onClick={handleClick}>
+        1
+      </button>
+      <button type="button" className="grid-item two" onClick={handleClick}>
+        2
+      </button>
+      <button type="button" className="grid-item three" onClick={handleClick}>
+        3
+      </button>
+      <button
+        type="button"
+        className="grid-item plus operation"
+        onClick={handleClick}
+      >
+        +
+      </button>
+      <button type="button" className="grid-item zero" onClick={handleClick}>
+        0
+      </button>
+      <button type="button" className="grid-item period" onClick={handleClick}>
+        .
+      </button>
+      <button
+        type="button"
+        className="grid-item equal operation"
+        onClick={handleClick}
+      >
+        =
+      </button>
+    </div>
+  );
+};
 
-  render() {
-    let val = 0;
-    const { total, next } = this.state;
-    if (next) {
-      val = next;
-    } else if (total) {
-      val = total;
-    }
-
-    return (
-      <div className="container">
-        <input
-          className="grid-item display"
-          placeholder="0"
-          value={val}
-          readOnly
-        />
-        <button
-          type="button"
-          className="grid-item AC"
-          onClick={this.handleClick}
-        >
-          AC
-        </button>
-        <button
-          type="button"
-          className="grid-item operations"
-          onClick={this.handleClick}
-        >
-          +/-
-        </button>
-        <button
-          type="button"
-          className="grid-item procentage"
-          onClick={this.handleClick}
-        >
-          %
-        </button>
-        <button
-          type="button"
-          className="grid-item divide operation"
-          onClick={this.handleClick}
-        >
-          ÷
-        </button>
-        <button
-          type="button"
-          className="grid-item seven"
-          onClick={this.handleClick}
-        >
-          7
-        </button>
-        <button
-          type="button"
-          className="grid-item eight"
-          onClick={this.handleClick}
-        >
-          8
-        </button>
-        <button
-          type="button"
-          className="grid-item nine"
-          onClick={this.handleClick}
-        >
-          9
-        </button>
-        <button
-          type="button"
-          className="grid-item multiply operation"
-          onClick={this.handleClick}
-        >
-          ×
-        </button>
-        <button
-          type="button"
-          className="grid-item four"
-          onClick={this.handleClick}
-        >
-          4
-        </button>
-        <button
-          type="button"
-          className="grid-item five"
-          onClick={this.handleClick}
-        >
-          5
-        </button>
-        <button
-          type="button"
-          className="grid-item six"
-          onClick={this.handleClick}
-        >
-          6
-        </button>
-        <button
-          type="button"
-          className="grid-item minus operation"
-          onClick={this.handleClick}
-        >
-          -
-        </button>
-        <button
-          type="button"
-          className="grid-item one"
-          onClick={this.handleClick}
-        >
-          1
-        </button>
-        <button
-          type="button"
-          className="grid-item two"
-          onClick={this.handleClick}
-        >
-          2
-        </button>
-        <button
-          type="button"
-          className="grid-item three"
-          onClick={this.handleClick}
-        >
-          3
-        </button>
-        <button
-          type="button"
-          className="grid-item plus operation"
-          onClick={this.handleClick}
-        >
-          +
-        </button>
-        <button
-          type="button"
-          className="grid-item zero"
-          onClick={this.handleClick}
-        >
-          0
-        </button>
-        <button
-          type="button"
-          className="grid-item period"
-          onClick={this.handleClick}
-        >
-          .
-        </button>
-        <button
-          type="button"
-          className="grid-item equal operation"
-          onClick={this.handleClick}
-        >
-          =
-        </button>
-      </div>
-    );
-  }
-}
+Calculator.propTypes = {
+  calcStates: PropTypes.instanceOf(Object).isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Calculator;
